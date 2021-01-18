a2917fdfbbb126afcb3bd0eb7dc8b0dc55129c4f
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-01-18 18:11:08 +0300

    Stage 2/5

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,7 +1,18 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        char[] line = scanner.nextLine().trim().toCharArray();
+        System.out.println("---------");
+
+        for (int i = 3; i <= line.length; i = i + 3) {
+            System.out.println("| " + line[i - 3] + " " + line[i - 2] + " " + line[i - 1] + " |");
+        }
+
+        System.out.println("---------");
     }
 }
